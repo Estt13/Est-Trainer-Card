e56efeaeae893119ffb321bd2ca60b176dabfba1
Est <httpestjc@gmail.com>
2024-07-27 05:26:06 -0300

    mudei tudo de lugar

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function toggleMode(){
     const img = document.querySelector("#profile img", )
     if (html.classList.contains("dark")) {
         
-        img.setAttribute("src", "./eu png.png", )
+        img.setAttribute("src", "./assets/eu png.png", )
 
     } 
      
     else { 
 
-        img.setAttribute("src", "./Est.png", "./Greninja-AttackAnimation-XY-3.webp")
+        img.setAttribute("src", "./assets/Est.png", "./assets/Greninja-AttackAnimation-XY-3.webp")
         
     }
 }
